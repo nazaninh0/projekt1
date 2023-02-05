1c12ccb7fd0ae4bfeca42e8ab1e7fb5df7188955
nazanin.heydari <nazanin.heydari@5CD132CZVV.learnet.se>
2023-02-06 00:24:27 +0100

    added some new codes

--- src/projekt_1/Projekt_1.java
+++ src/projekt_1/Projekt_1.java
@@ -4,6 +4,7 @@
  */
 package projekt_1;
 
+import static java.lang.Math.random;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -20,27 +21,109 @@ public class Projekt_1 {
         // TODO code application logic here
         Scanner scan=new Scanner(System.in); 
         Random rand= new Random();
-        String[] fiende= {"dator "}; 
+        String[] fiende= {"Opponent"}; 
         int maxfiendehälso = 100 ; 
         int health = 100; 
+        // while loop 
         boolean running = true; 
-        System.out.println("välkoman till spelet ");
+        System.out.println("!!-----------welcome to the game-----------!!");
+        System.out.println("It is text based game between you and the opponent \n You attack by pressing the enter on the keyboard \n You have recieved 100 HP");
+        scan.nextLine();
+        System.out.println("Enter your name ");
+        String namn= scan.nextLine();
+        for (String opponent: fiende){
+           System.out.println(opponent +"appeard");
+        }
         GAME : 
         while(running){
             int max= 100; 
             int min= 1; 
-            int x = (int)(Math.random()*(max-min+1)+min); 
+            // slumpa ett tal mellan 1-100 för att avgöra ifall det är opponents tur eller spelaren 
+            int x = (int)(Math.random()*(max-min+1)+min);
             if(x>50){
-                System.out.println("det är din tur att attackera ");
+                System.out.println(namn + " It is your turn to attack ");
+                System.out.println("press the enter ");
+                scan.nextLine();
+                // slumpa ett tal för att se om en attack ska lycka eller inte 
+                int attacklyckas = (int)(Math.random()*(max-min+1)+min); 
+                //om slump talet är mindre än 25 attacket kommer lyckas 
+                if ( attacklyckas < 25)
+                {
+                  int spelare = 1 + (int)(Math.random()*100);
+                  int dator = 1+ (int)(Math.random()*100); 
+                  // differesen mellan dator och spelare slump tal 
+                  int antalhp= dator-spelare; 
+                  // hälso poängen miskar med antalhp 
+                  health-=antalhp;  
+                  System.out.println("attack succed "+ " you have "+ health +" HP " );
+                }
+                else{
+                    // attack lyckas inte (slumpa tal är större än 25)
+                    System.out.println("attack didnt succe");
+                }
+                             
+                if (health<30){
+                  //spelaren dött
+                  System.out.println("You dont have enough HP to continue!!");
+                  System.out.println("opponent won ");
+                  break GAME;
+                }
             }
             else{
-                System.out.println("det är datorns tur att attckera");
-                    
+                // opponent tur 
+              System.out.println("It is opponenet turn to attack!!"); 
+                System.out.println("press the enter");
+                scan.nextLine(); 
+              int attacklyckas = (int)(Math.random()*(max-min+1)+min);
+              
+              if (attacklyckas<25){
+                  int spelare = 1 + (int)( Math.random()*100);
+                  int dator = 1 + (int) (Math.random()*100); 
+                  int hp1= dator -spelare ; 
+                  // opponents hälso poäng efter att attcken lyckades 
+                  maxfiendehälso-=hp1 ; 
+                  System.out.println("attack succed "+" opponent have "+ maxfiendehälso +" HP ");   
+              }
+              else {
+                  // attack lyckas inte 
+                  System.out.println("attack didnt sucess" );
+              }
+              if (maxfiendehälso<30){
+                  // opponent dött
+                 System.out.println("Congrats "+ namn + " you won" );
+                 break GAME;
+              }
             }
-            break GAME; 
+          /*System.out.println("!!!-----------------------------------!!!");
+          System.out.println("What would you like to do now");
+          System.out.println("1.Continue playing \n 2: stop ");
+          int svar= scan.nextInt(); 
+          int svar1 = 1; 
+          int svar2= 2;
+          try{
+              svar1 = scan.nextInt();
+              svar2= scan.nextInt();
+          }
+          catch(Exception e){
+              System.out.println("You need enter a number not a word");
+          }
+          if (svar==1){
+             System.out.println("You continue playing");
             }
-        
+          else{
+             System.out.println("You stoped the game ");
+             break; 
+            } */            
+
         }
+        
+        System.out.println("!!-------------------!!"); 
+         
+        System.out.println(" thanks for playing");
+         
+        }
+        
     }
+    
     
 
